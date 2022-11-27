b82f852e433c35f184130d1df9954f692c08c58f
Kosuke Miyashita <kosuke.miyashita@gmail.com>
2022-11-27 18:59:16 +0900

    [Fix] Lintチェック後の修正 #21

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     sequence(:name) { |n| "user_#{n}" }
     sequence(:email) { |n| "user_#{n}@example.com" }
-    password { "password" }
-    password_confirmation { "password" }
+    password { 'password' }
+    password_confirmation { 'password' }
   end
 end
