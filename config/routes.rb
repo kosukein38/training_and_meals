7bf51b3e0ab69ea4b3135405fcfd15f90e756be8
Kosuke Miyashita <kosuke.miyashita@gmail.com>
2022-11-27 18:03:04 +0900

    [Add] usersコントローラー、ルーティング、ビューファイル #21

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   root to: 'static_pages#top'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: %i[new create]
 end
