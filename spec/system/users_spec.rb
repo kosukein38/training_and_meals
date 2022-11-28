91b2fcdb4b65f5b398f593f1c800144b2f4231c4
Kosuke Miyashita <kosuke.miyashita@gmail.com>
2022-11-28 22:27:45 +0900

    [Add] ユーザー登録の統合テスト追加 #21

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+  
+  let(:user) { create(:user) }
+
+  describe 'ユーザー新規登録' do
+    context 'フォームの入力値が正常' do
+      it 'ユーザーの新規作成が成功する' do
+        visit new_user_path
+        fill_in 'Name', with: 'Example'
+        fill_in 'Email', with: 'email@example.com'
+        fill_in 'Password', with: 'password'
+        fill_in 'Password confirmation', with: 'password'
+        click_button '登録する'
+        expect(current_path).to eq root_path
+      end
+    end
+    context 'メールアドレスが未入力' do
+      it 'ユーザーの新規作成が失敗する' do
+        visit new_user_path
+        fill_in 'Name', with: 'Example'
+        fill_in 'Email', with: ''
+        fill_in 'Password', with: 'password'
+        fill_in 'Password confirmation', with: 'password'
+        click_button '登録する'
+        expect(current_path).to eq users_path
+      end
+    end
+    context '登録済のメールアドレスを使用' do
+      it 'ユーザーの新規作成が失敗する' do
+        existed_user = create(:user)
+        visit new_user_path
+        fill_in 'Name', with: 'Example'
+        fill_in 'Email', with: existed_user.email
+        fill_in 'Password', with: 'password'
+        fill_in 'Password confirmation', with: 'password'
+        click_button '登録する'
+        expect(current_path).to eq users_path
+      end
+    end
+  end
+end
