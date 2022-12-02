f94fbeb2d8d0d85986eea67883ee08405412190c
Kosuke Miyashita <kosuke.miyashita@gmail.com>
2022-12-02 22:24:16 +0900

    [Modify] beforeアクションによるユーザーの認可 #23

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
+  before_action :require_login
+
+  def not_authenticated
+    flash[:warning] = 'ログインしてください'
+    redirect_to login_path
+  end
 end
