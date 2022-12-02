b17ebd426a7820901f21c68dee4dd121cd279aaf
Kosuke Miyashita <kosuke.miyashita@gmail.com>
2022-12-02 22:08:11 +0900

    [Add] profilesコントローラー、ビュー、ルーティングの追加 #23

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,26 @@
+class ProfilesController < ApplicationController
+  before_action :set_user, only: %i[edit update]
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to profile_path, success: '情報を更新しました'
+    else
+      flash.now['danger'] = '更新できませんでした'
+      render :edit
+    end
+  end
+
+  def show; end
+
+  private
+
+  def set_user
+    @user = User.find(current_user.id)
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :name)
+  end
+end
