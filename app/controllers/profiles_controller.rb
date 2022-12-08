cb0162303021d5e1b649019be0302ae5cb0419cb
Kosuke Miyashita <kosuke.miyashita@gmail.com>
2022-12-08 22:09:33 +0900

    [Add] profilesコントローラーとeditビューの追加 #23

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,7 +5,15 @@ class ProfilesController < ApplicationController
 
   def update
     if @user.update(user_params)
-      redirect_to profile_path, success: '情報を更新しました'
+      set_coefficient
+      if @user.male?
+        @user.maintenance_calorie = (13.397 * @user.body_weight + 4.799 * @user.height - 5.677 * @user.age + 88.362) * @coefficient
+      else
+        @user.maintenance_calorie = (9.247 * @user.body_weight + 3.098 * @user.height - 4.33 * @user.age + 447.593) * @coefficient
+      end
+      @user.target_calorie = ( @user.maintenance_calorie + user_params[:adjustment_calorie].to_i + 500 )
+      @user.save!
+      redirect_to edit_profile_path, success: '情報を更新しました'
     else
       flash.now['danger'] = '更新できませんでした'
       render :edit
@@ -16,11 +24,28 @@ class ProfilesController < ApplicationController
 
   private
 
+  def set_coefficient
+    case @user.active_level
+    when 'level1'
+      @coefficient = 1.2
+    when 'level2'
+      @coefficient = 1.375
+    when 'level3'
+      @coefficient = 1.55
+    when 'level4'
+      @coefficient = 1.725
+    when 'level5'
+      @coefficient = 1.9
+    else
+      @coefficient = 1
+    end
+  end
+
   def set_user
     @user = User.find(current_user.id)
   end
 
   def user_params
-    params.require(:user).permit(:email, :name)
+    params.require(:user).permit(:email, :name, :introduction, :height, :body_weight, :age, :sex, :active_level, :target_weight, :target_date, :adjustment_calorie, :twitter_link, :facebook_link, :instagram_link)
   end
 end
