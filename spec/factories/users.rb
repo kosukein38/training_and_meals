753c40118c21b4fac9fbbab64a7a8ac3b8adb03a
Kosuke Miyashita <kosuke.miyashita@gmail.com>
2022-12-08 22:10:11 +0900

    [Add] プロフィールのシステムテスト追加 #23

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,10 @@ FactoryBot.define do
     sequence(:email) { |n| "user_#{n}@example.com" }
     password { 'password' }
     password_confirmation { 'password' }
+    height { 163.5 }
+    body_weight { 85 }
+    age { 32 }
+    sex { 'male' }
+    active_level { 'level1' }
   end
 end
---
+++ spec/system/profiles_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "Profiles", type: :system do
+  let(:user) { create(:user) }
+
+  it 'プロフィール登録するすると、メンテナンスカロリーと目標カロリーが登録され、表示されること' do
+    login_as(user)
+    visit edit_profile_path
+    fill_in 'Height', with: user.height
+    fill_in 'Body weight', with: user.body_weight
+    fill_in 'Age', with: user.age
+    select 'male', from: 'Sex'
+    select 'level1', from: 'Active level'
+    click_button '計算する'
+    expect(page).to have_content 'あなたの目標摂取カロリーは１日あたり'
+    expect(page).to have_current_path edit_profile_path, ignore_query: true
+  end
+end
