cfa7dec47b1e6cd476f337cdac2d8227a46bad30
Kosuke Miyashita <kosuke.miyashita@gmail.com>
2022-12-10 15:50:47 +0900

    [Fix] createアクション, [Add] ユーザー登録時のエラー表示 #21

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to login_path
+      redirect_to login_path, success: '登録しました'
     else
-      render :new
+      flash.now['danger'] = '登録できませんでした'
+      render :new, status: :unprocessable_entity
     end
   end
 
