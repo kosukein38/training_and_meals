a919911defb2a5b75005590199ed5ad5ddfa1844
Kosuke Miyashita <kosuke.miyashita@gmail.com>
2022-12-18 10:59:23 +0900

    [Fix] トップページに遷移時のバグ修正 #47

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,12 @@
 class StaticPagesController < ApplicationController
   skip_before_action :require_login
+  before_action :set_user
 
-  def top; end
+  def top ;end
+
+  private
+
+  def set_user
+    @user = User.find(current_user.id) if logged_in?
+  end
 end
