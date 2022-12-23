e3bbfca3b652b3e360e1058e2057f09864f80070
Kosuke Miyashita <kosuke.miyashita@gmail.com>
2022-12-23 19:57:34 +0900

    [Add] updateアクション、ビュー、ルーティングの追加 #68

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,5 +1,5 @@
 class WorkoutsController < ApplicationController
-  before_action :set_user, only: %i[new show create, edit]
+  before_action :set_user, only: %i[new show create edit update]
   before_action :set_workout, only: %i[show edit]
 
   def show; end
@@ -22,11 +22,26 @@ class WorkoutsController < ApplicationController
     @workout_form = Workout.find(params[:id])
   end
 
+  def update
+    @workout_form = WorkoutForm.new(workout_params)
+    if @workout_form.update
+      redirect_to user_path(@user.id), success: t('defaults.message.updated', item: Workout.model_name.human)
+    else
+      flash.now['danger'] = t('defaults.message.not_updated', item: Workout.model_name.human)
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def workout_form_params
     params.require(:workout_form).permit(:workout_date, :workout_title, :workout_time, :workout_weight, :repetition_count,
                                          :set_count, :workout_memo, body_part_ids: []).merge(user_id: current_user.id)
+  end
+
+  def workout_params
+    params.require(:workout).permit(:workout_date, :workout_title, :workout_time, :workout_weight, :repetition_count,
+                                         :set_count, :workout_memo, body_part_ids: []).merge(user_id: current_user.id, workout_id: params[:id])
   end
 
   def set_user
