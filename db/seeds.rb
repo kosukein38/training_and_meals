ee41897b6d31daefc7651fe5a3705652bbec279a
Kosuke Miyashita <kosuke.miyashita@gmail.com>
2022-12-28 22:43:00 +0900

    [Fix] seedファイルの読み込み設定修正 #77

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,2 @@
-#筋トレ部位のデータ
-body_part_names = [ "胸", "肩", "背中", "腕", "脚"]
-body_part_names.each do |body_part_name|
-  BodyPart.create!(body_part_name: body_part_name)
-end
-
 #環境別のseedファイル読み込み
 load(Rails.root.join("db", "seeds", "#{Rails.env.downcase}.rb"))
--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,3 +1,9 @@
+#筋トレ部位のデータ
+body_part_names = [ "胸", "肩", "背中", "腕", "脚"]
+body_part_names.each do |body_part_name|
+  BodyPart.create!(body_part_name: body_part_name)
+end
+
 # メインのサンプルユーザー
 User.create!(
   name:  "FirstUser",
