2aaac5e6c98c925531f74a5865b69e61ca8ffda5
Kosuke Miyashita <kosuke.miyashita@gmail.com>
2022-12-28 14:14:22 +0900

    [Fix] Lintチェック指摘修正 #75

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -2,8 +2,14 @@ class MealsController < ApplicationController
   before_action :set_user, only: %i[new create show edit update destroy]
   before_action :set_meal, only: %i[show edit destroy]
 
+  def show; end
+
   def new
     @meal_form = MealForm.new
+  end
+
+  def edit
+    @meal_form = Meal.find(params[:id])
   end
 
   def create
@@ -16,19 +22,12 @@ class MealsController < ApplicationController
     end
   end
 
-  def show ;end
-
-  def edit
-    @meal_form = Meal.find(params[:id])
-  end
-
   def update
     @meal_form = MealForm.new(meal_params)
 
     if @meal_form.update
       redirect_to user_path(@user.id), success: t('defaults.message.updated', item: Meal.model_name.human)
     else
-      debugger
       flash.now['danger'] = t('defaults.message.not_updated', item: Meal.model_name.human)
       render :edit, status: :unprocessable_entity
     end
@@ -50,9 +49,9 @@ class MealsController < ApplicationController
 
   def meal_params
     params.require(:meal).permit(:meal_period, :meal_type, :meal_memo,
-                                      :meal_title_first, :meal_weight_first, :meal_calorie_first,
-                                      :meal_title_second, :meal_weight_second, :meal_calorie_second,
-                                      :meal_title_third, :meal_weight_third, :meal_calorie_third).merge(user_id: current_user.id, meal_id: params[:id])
+                                 :meal_title_first, :meal_weight_first, :meal_calorie_first,
+                                 :meal_title_second, :meal_weight_second, :meal_calorie_second,
+                                 :meal_title_third, :meal_weight_third, :meal_calorie_third).merge(user_id: current_user.id, meal_id: params[:id])
   end
 
   def set_user
