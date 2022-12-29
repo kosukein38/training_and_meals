cd2753f2bab9434049dc9c445616ffaa02ce7777
Kosuke Miyashita <kosuke.miyashita@gmail.com>
2022-12-29 23:12:55 +0900

    [Add] apiコントローラー、ビュー、設定、テストファイルの追加 #26

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   resources :meals, only: %i[show new create edit update destroy]
   get 'home/workouts', to: 'home#workouts'
   get 'home/meals', to: 'home#meals'
+  get 'result', to: 'api#result'
 end
