a9cf15afef09e4ae351b1873018a628c382b9b91
Kosuke Miyashita <kosuke.miyashita@gmail.com>
2022-12-31 22:49:19 +0900

    [Fix] Lintチェックの指摘点修正 #26

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -45,22 +45,22 @@ class MealsController < ApplicationController
 
   def calorie_search
     text = params[:calorie_search]
-    #APIキーを定義
+    # APIキーを定義
     deepl_api_key = Rails.application.credentials.deepl[:api_key]
-    #リクエストURLを定義
-    deepl_api_url = URI("https://api-free.deepl.com/v2/translate")
-    #リクエストパラメーターを定義
+    # リクエストURLを定義
+    deepl_api_url = URI('https://api-free.deepl.com/v2/translate')
+    # リクエストパラメーターを定義
     req_params = {
       auth_key: deepl_api_key,
-      text: text,
-      target_lang: "EN",
-      source_lang: "JA"
+      text:,
+      target_lang: 'EN',
+      source_lang: 'JA'
     }
-    #リクエストを送信し、返ってきたレスポンスを変数responseに格納
+    # リクエストを送信し、返ってきたレスポンスを変数responseに格納
     response = Net::HTTP.post_form(deepl_api_url, req_params)
-    result = JSON.parse(response.body)
-    translated_text = result["translations"][0]["text"]
-    #translated_textをクエリに含ませ、Nutrition by API-NinjasのAPIを叩く
+    result_body = JSON.parse(response.body)
+    translated_text = result_body['translations'][0]['text']
+    # translated_textをクエリに含ませ、Nutrition by API-NinjasのAPIを叩く
     nutrition_url = URI("https://nutrition-by-api-ninjas.p.rapidapi.com/v1/nutrition?query=#{translated_text}")
 
     http = Net::HTTP.new(nutrition_url.host, nutrition_url.port)
@@ -68,14 +68,14 @@ class MealsController < ApplicationController
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
     request = Net::HTTP::Get.new(nutrition_url)
-    request["X-RapidAPI-Key"] = Rails.application.credentials.nutrition[:api_key]
-    request["X-RapidAPI-Host"] = 'nutrition-by-api-ninjas.p.rapidapi.com'
+    request['X-RapidAPI-Key'] = Rails.application.credentials.nutrition[:api_key]
+    request['X-RapidAPI-Host'] = 'nutrition-by-api-ninjas.p.rapidapi.com'
 
     response = http.request(request)
     results = JSON.parse(response.body)
     final_results = []
     results.each do |result|
-      final_results << "メニュー：#{result["name"]} カロリー：#{result["calories"]} kcal グラム数：#{result["serving_size_g"]} g"
+      final_results << "メニュー：#{result['name']} カロリー：#{result['calories']} kcal グラム数：#{result['serving_size_g']} g"
     end
     @response = final_results
     @meal_form = MealForm.new
