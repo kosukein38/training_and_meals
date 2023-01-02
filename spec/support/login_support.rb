6ebe261c46fdd6e9809c961ee56f5783298810ce
Kosuke Miyashita <kosuke.miyashita@gmail.com>
2023-01-02 23:37:19 +0900

    [Fix] システムテスト対応 #89

--- spec/support/login_support.rb
+++ spec/support/login_support.rb
@@ -1,7 +1,6 @@
 module LoginSupport
   def login_as(user)
-    visit root_path
-    click_link 'ログイン'
+    visit login_path
     fill_in 'メールアドレス', with: user.email
     fill_in 'パスワード', with: 'password'
     click_button 'ログイン'
