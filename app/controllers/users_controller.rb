857620e1ab3b5bfbeb62c7772b1d6a9b0f035644
Kosuke Miyashita <kosuke.miyashita@gmail.com>
2023-01-08 19:20:24 +0900

    [Fix] users#showのビュー修正、[Modify] users#showページに未ログインユーザーもアクセスできるように修正 #104

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :require_login, only: %i[new create]
+  skip_before_action :require_login, only: %i[new create show]
 
   def show
     @user = User.find(params[:id])
