ac5f6d844db6558d1f6b5a315adc7adf8e33ab98
Kosuke Miyashita <kosuke.miyashita@gmail.com>
2023-01-29 14:57:24 +0900

    [Add] meta-tagsの追加, ogp画像、favicon画像の追加 #131

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,46 @@
 module ApplicationHelper
+  def show_meta_tags
+    if display_meta_tags.blank?
+      assign_meta_tags
+    end
+    display_meta_tags
+  end
+
+  def assign_meta_tags(options = {})
+    defaults = t('meta_tags.defaults')
+    options.reverse_merge!(defaults)
+
+    site = options[:site]
+    title = options[:title]
+    description = options[:description]
+    keywords = options[:keywords]
+    image = options[:image].presence || image_url('ogp-image.png')
+
+    configs = {
+      separator: '|',
+      reverse: true,
+      site: site,
+      title: title,
+      description: description,
+      keywords: keywords,
+      canonical: request.original_url,
+      icon: { 
+        href: image_url('favicon.png') 
+      },
+      og: {
+        type: 'website',
+        title: title.presence || site,
+        description: description,
+        url: request.original_url,
+        image: image,
+        site_name: site
+      },
+      twitter: {
+        site: '@kosukein38',
+        card: 'summary',
+        image: image
+      }
+    }
+    set_meta_tags(configs)
+  end
 end
