8cbb1346476f40748725e52f8a72b052157dc879
Kosuke Miyashita <kosuke.miyashita@gmail.com>
2023-01-31 21:40:43 +0900

    [Add] 本番環境用のメール設定追加 #38

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,24 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.default_url_options = { host: Settings.default_url_options[:host] }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.default :charset => 'utf-8'
+  
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    port: 587,
+    user_name: Rails.application.credentials.gmail[:user_name],
+    password: Rails.application.credentials.gmail[:password],
+    authentication: :login
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
