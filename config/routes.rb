947f05417cedee89c44707632472043d34bb62d2
Kosuke Miyashita <kosuke.miyashita@gmail.com>
2023-02-05 17:18:13 +0900

    [Add] フォロー中ユーザーの筋トレ投稿一覧機能追加 #143

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   resource :profile, only: %i[show edit update destroy]
   resources :workouts
   resources :meals
+  get 'workouts_feed', to: 'workouts#workouts_feed'
   get 'calorie_search', to: 'meals#calorie_search'
   get 'date_search', to: 'users#date_search'
   get 'sitemap', to: redirect("https://s3-ap-northeast-1.amazonaws.com/#{Rails.application.credentials.aws[:s3][:bucket]}/sitemaps/sitemap.xml.gz")
