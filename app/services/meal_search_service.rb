664643d2071fee85cf04c559c3f8e41269d4cd04
Kosuke Miyashita <kosuke.miyashita@gmail.com>
2023-02-09 18:08:39 +0900

    [Fix] Lintチェックの指摘修正 #173

--- app/services/meal_search_service.rb
+++ app/services/meal_search_service.rb
@@ -16,7 +16,7 @@ class MealSearchService
   attr_reader :text
 
   def search_meal
-    #Deepl APIで日本語入力を英語に
+    # Deepl APIで日本語入力を英語に
     deepl_api_uri = URI.parse('https://api-free.deepl.com/v2/translate')
     deepl_api_key = Rails.application.credentials.deepl[:api_key]
     req_params = {
@@ -28,19 +28,19 @@ class MealSearchService
     deepl_response = Net::HTTP.post_form(deepl_api_uri, req_params)
     deepl_response_body = JSON.parse(deepl_response.body)
     translated_text = deepl_response_body['translations'][0]['text']
-    #英語に翻訳された'translated_text'をNutrition by API-Ninjas APIに渡して、カロリーを取得
+    # 英語に翻訳された'translated_text'をNutrition by API-Ninjas APIに渡して、カロリーを取得
     nutrition_api_uri = URI.parse("https://nutrition-by-api-ninjas.p.rapidapi.com/v1/nutrition?query=#{translated_text}")
     nutrition_api_key = Rails.application.credentials.nutrition[:api_key]
-    headers = { 
+    headers = {
       'X-RapidAPI-Key' => nutrition_api_key,
-      'X-RapidAPI-Host'  => nutrition_api_uri.host 
+      'X-RapidAPI-Host' => nutrition_api_uri.host
     }
     http = Net::HTTP.new(nutrition_api_uri.host, nutrition_api_uri.port)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     nutrition_response = http.get(nutrition_api_uri, headers)
     nutrition_response_body = JSON.parse(nutrition_response.body)
-    #resultsに結果を入れて返す
+    # resultsに結果を入れて返す
     results = []
     nutrition_response_body.each do |result|
       results << "メニュー：#{result['name']} カロリー：#{result['calories']} kcal グラム数：#{result['serving_size_g']} g"
