874bf1691afccc6a38d30f1499e1278580b227e6
Kosuke Miyashita <kosuke.miyashita@gmail.com>
2023-03-08 22:19:51 +0900

    [Modify] 筋トレ投稿いいねボタンのTurbo Stream適用 #154

--- app/controllers/workout_likes_controller.rb
+++ app/controllers/workout_likes_controller.rb
@@ -5,14 +5,20 @@ class WorkoutLikesController < ApplicationController
   end
 
   def create
-    workout = Workout.find(params[:workout_id])
-    current_user.like_workout(workout)
-    redirect_to request.referer
+    @workout = Workout.find(params[:workout_id])
+    current_user.like_workout(@workout)
+    respond_to do |format|
+      format.html { redirect_to request.referer }
+      format.turbo_stream
+    end
   end
 
   def destroy
-    workout = current_user.workout_likes.find(params[:id]).workout
-    current_user.unlike_workout(workout)
-    redirect_to request.referer
+    @workout = current_user.workout_likes.find(params[:id]).workout
+    current_user.unlike_workout(@workout)
+    respond_to do |format|
+      format.html { redirect_to request.referer }
+      format.turbo_stream
+    end
   end
 end
